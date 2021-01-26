858bac7ee66e0944e6489769a010494f652d3b70
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2021-01-27 00:01:59 +0900

    ToDo_list

---
+++ task.rb
@@ -0,0 +1,14 @@
+class Task
+  attr_reader :id
+  @@count = 0
+  def initialize(title:, content:)
+    @@count += 1
+    @id = @@count
+    @title = title
+    @content = content
+  end
+
+  def info
+    "[No.#{@id}] #{@title}:#{@content}"
+  end
+end
